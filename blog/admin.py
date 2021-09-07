1afb3dff5324d3f16d22e88eeffd8396a92142a1
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-09-07 22:27:02 +0530

    serializers

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Blog, Author, Entry
 # Register your models here.
+admin.site.register(Entry)
+admin.site.register(Author)
+admin.site.register(Blog)
