224d51ed59d464fbbc551911bd0ae800b2d1c7df
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-09-16 18:12:53 +0530

    postgresql config

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 class Blog(models.Model):
     name = models.CharField(max_length=100)
     tagline = models.TextField()
@@ -14,7 +15,7 @@ class Entry(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     headline = models.CharField(max_length=255,unique=True)
     content = models.TextField()
-    subtopics = models.TextField()
+    subtopics = ArrayField(models.CharField(max_length=200))
     pub_date = models.DateField(auto_now_add=True)
     mod_date = models.DateField(auto_now_add= True)
     authors = models.ManyToManyField(Author)
